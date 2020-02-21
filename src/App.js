30e321d85bdf50c77a056b6cea9c13319f445763
AmirArreaza <ajaarreazav@gmail.com>
2020-02-21 10:31:53 +0000

    Added line to bother my wife

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cambios
+          Hola Mamosito pesiosa!
+          Dame un besito
         </p>
         <a
           className="App-link"
